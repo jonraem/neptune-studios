1a2c79efd2acdca850059ec4bcd628dec74328a2
Joni Rämö <joni.ramo.91@gmail.com>
2020-03-28 15:09:45 +0200

    Flesh out about page hero

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Header from '../components/header';
+import styles from './pages.module.css';
 
-export default () =>
+export default (props) => 
   (
-    <div>
-      <Header />
-      About
+    <div className={styles.page}>
+      <Header currentPath={props.location.pathname} />
+      <div className={styles.aboutHero}>
+        <div className={styles.heroText}>
+          <div className={styles.heroTitles}>
+            <div className={styles.subtitle}>About</div>
+            <h1>Neptune's finest</h1>
+          </div>
+          <div className={styles.subtitle}>
+            Neptune Studios is a one man’s vision and voyage in the seas of
+            design and problem solving.
+          </div>
+        </div>
+      </div>
     </div>
   );
