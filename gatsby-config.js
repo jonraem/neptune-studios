edde339bacfdf6a0d6d94dbbe5d4b997e8fcd031
Joni Rämö <joni.ramo.91@gmail.com>
2020-03-28 15:26:13 +0200

    Remove gatsby-plugin-google-fonts and import fonts directly to css instead

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,16 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `Open Sans\:300,400,600`
-        ],
-        display: 'swap'
-      }
-    }
-  ]
-}
+module.exports = { };
