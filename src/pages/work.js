08af2d746d4a24822df8a7cfd73f85864d482c94
Joni Rämö <joni.ramo.91@gmail.com>
2020-03-28 15:06:06 +0200

    Flesh out work page hero

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Header from '../components/header';
+import styles from './pages.module.css';
 
-export default () =>
+export default (props) => 
   (
-    <div>
-      <Header />
-      Work
+    <div className={styles.page}>
+      <Header currentPath={props.location.pathname} />
+      <div className={styles.workHero}>
+        <div className={styles.heroText}>
+          <div className={styles.heroTitles}>
+            <div className={styles.subtitle}>What has</div>
+            <h1>happened lately</h1>
+          </div>
+          <div className={styles.subtitle}>(Or selected works).</div>
+          <div>
+            Eventhough digital product design has taken most of the time
+            nowadays, this section also presents some examples from the graphic
+            design and branding field, illustration and photography.
+          </div>
+        </div>
+      </div>
     </div>
   );
