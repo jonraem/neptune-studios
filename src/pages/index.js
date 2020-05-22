d7764595a168973fecd29266a31c03a816e3482c
Joni Rämö <joni.ramo.91@gmail.com>
2020-05-22 15:36:22 +0300

    Flesh out styles for main page (excluding case cards)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import CenterTitle from '../components/centerTitle';
 import styles from './pages.module.css';
 
 export default props => (
@@ -19,6 +20,40 @@ export default props => (
           The driving forces behind Neptune Studios. No setting sails for rough
           seas or taking dives to the deep-end without proper preparation.
         </p>
+      </div>
+    </div>
+    <div className={styles.content}>
+      <div>
+        <CenterTitle>
+          Neptune Studios <br /> can help you with:
+        </CenterTitle>
+        <div className={styles.mainHelp}>
+          <p className={styles.mainHelpPara}>
+            The goal is to help clients achieve the best solution with a design
+            related problem. Functionality and practicality does not have to
+            mean “dull” or “My nephew could’ve done it!”. On the right side is
+            presented part of the skill set I’ve worked with.
+          </p>
+          <div className={styles.divider} />
+          <div className={styles.mainHelpItems}>
+            <div className={styles.mainHelpItem}>User interface design</div>
+            <div className={styles.mainHelpItem}>User experience design</div>
+            <div className={styles.mainHelpItem}>Workshops</div>
+            <div className={styles.mainHelpItem}>User testing</div>
+            <div className={styles.mainHelpItem}>Prototypes</div>
+            <div className={styles.mainHelpItem}>Visual design</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.mainPeek}>
+        <div className={styles.fullWidthGrey} />
+        <h3>Have a peek</h3>
+        <div>Some of the selected works from different creative fields.</div>
+        <div>
+          <div></div> {/* Case cards */}
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
     <Footer />
