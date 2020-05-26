f5994de87f51783cc19ea9475f7e9f3953d7fa7b
Joni Rämö <joni.ramo.91@gmail.com>
2020-05-26 18:00:58 +0300

    Add Menu component and styling

---
+++ src/components/menu.js
@@ -0,0 +1,60 @@
+import React, { useContext } from 'react';
+import { Link } from 'gatsby';
+import MenuContext from '../context/MenuContext';
+import { ReactComponent as Cross } from '../assets/svg/cross.svg';
+import styles from './components.module.css';
+
+export default props => {
+  const { setIsOpen } = useContext(MenuContext);
+
+  const getActiveBorder = pathName => {
+    if (props.currentPath === pathName) {
+      return { borderBottom: '3px solid #0e0c3c' };
+    } else return {};
+  };
+
+  return (
+    <div className={styles.menu}>
+      <div className={styles.menuLinks}>
+        <Link
+          to="/"
+          onClick={() => setIsOpen(false)}
+          className={styles.menuLink}
+          style={getActiveBorder('/')}
+        >
+          Main
+        </Link>
+        <Link
+          to="/work"
+          onClick={() => setIsOpen(false)}
+          className={styles.menuLink}
+          style={getActiveBorder('/work')}
+        >
+          Work
+        </Link>
+        <Link
+          to="/about"
+          onClick={() => setIsOpen(false)}
+          className={styles.menuLink}
+          style={getActiveBorder('/about')}
+        >
+          About
+        </Link>
+        <Link
+          to="/contact"
+          onClick={() => setIsOpen(false)}
+          className={styles.menuLink}
+          style={getActiveBorder('/contact')}
+        >
+          Contact
+        </Link>
+      </div>
+      <button
+        className={styles.menuCloseButton}
+        onClick={() => setIsOpen(false)}
+      >
+        <Cross />
+      </button>
+    </div>
+  );
+};
