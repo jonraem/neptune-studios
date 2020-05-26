9de8400f63aea3a03b96fefc6bed9876a15ef60d
Joni Rämö <joni.ramo.91@gmail.com>
2020-05-26 18:01:22 +0300

    Add hamburger to Header based on media query

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'gatsby';
+import MediaContext from '../context/MediaContext';
+import MenuContext from '../context/MenuContext';
 import { ReactComponent as NeptuneStudios } from '../assets/svg/neptune-studios--white.svg';
+import { ReactComponent as Hamburger } from '../assets/svg/hamburger.svg';
 import styles from './components.module.css';
 
 export default props => {
+  const { isMobile } = useContext(MediaContext);
+  const { setIsOpen } = useContext(MenuContext);
+
   const getActiveBorder = pathName => {
     if (props.currentPath === pathName) {
       return { borderBottom: '3px solid #fff' };
@@ -13,32 +19,45 @@ export default props => {
   return (
     <div className={styles.header}>
       <NeptuneStudios className={styles.headerLogo} />
-      <div className={styles.headerLinks}>
-        <Link to="/" className={styles.headerLink} style={getActiveBorder('/')}>
-          Main
-        </Link>
-        <Link
-          to="/work"
-          className={styles.headerLink}
-          style={getActiveBorder('/work')}
+      {isMobile ? (
+        <button
+          className={styles.unstyledButton}
+          onClick={() => setIsOpen(true)}
         >
-          Work
-        </Link>
-        <Link
-          to="/about"
-          className={styles.headerLink}
-          style={getActiveBorder('/about')}
-        >
-          About
-        </Link>
-        <Link
-          to="/contact"
-          className={styles.headerLink}
-          style={getActiveBorder('/contact')}
-        >
-          Contact
-        </Link>
-      </div>
+          <Hamburger className={styles.headerHamburger} />
+        </button>
+      ) : (
+        <div className={styles.headerLinks}>
+          <Link
+            to="/"
+            className={styles.headerLink}
+            style={getActiveBorder('/')}
+          >
+            Main
+          </Link>
+          <Link
+            to="/work"
+            className={styles.headerLink}
+            style={getActiveBorder('/work')}
+          >
+            Work
+          </Link>
+          <Link
+            to="/about"
+            className={styles.headerLink}
+            style={getActiveBorder('/about')}
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            className={styles.headerLink}
+            style={getActiveBorder('/contact')}
+          >
+            Contact
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
