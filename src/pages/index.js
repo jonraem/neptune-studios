b0ad025afd6db8f3bc75f4caab31aeca0f878903
Joni Rämö <joni.ramo.91@gmail.com>
2020-05-26 17:31:55 +0300

    Add MediaContext and MenuContext handling in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { get } from 'lodash';
+import Menu from '../components/menu';
 import Main from './main';
+import MediaContext from '../context/MediaContext';
+import MenuContext from '../context/MenuContext';
 import 'focus-visible';
 
-export default props => <Main {...props} />;
+export default props => {
+  const { setIsMobile, setIsTablet } = useContext(MediaContext);
+  const { isOpen } = useContext(MenuContext);
+
+  // Custom media query hook
+  const useMedia = (query, matchMedia = () => {}) => {
+    const [matches, setMatches] = useState(get(matchMedia(query), 'matches'));
+    useEffect(() => {
+      const media = matchMedia(query);
+      if (media.matches !== matches) setMatches(media.matches);
+      const listener = () => setMatches(media.matches);
+      media.addListener(listener);
+      return () => media.removeListener(listener);
+    }, [matches, query, matchMedia]);
+    return matches;
+  };
+
+  const isMobile = useMedia('(max-width: 450px)', window.matchMedia);
+  const isTablet = useMedia('(max-width: 1024px)', window.matchMedia);
+
+  useEffect(() => {
+    setIsMobile(isMobile);
+    setIsTablet(isTablet);
+  }, [isMobile, isTablet, setIsMobile, setIsTablet]);
+
+  return (
+    <>
+      {isOpen && <Menu />}
+      <Main {...props} />
+    </>
+  );
+};
