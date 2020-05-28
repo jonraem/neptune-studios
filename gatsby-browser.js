128895f220af23e24e996038f9939a56e64c2d18
Joni Rämö <joni.ramo.91@gmail.com>
2020-05-28 14:34:36 +0300

    Separate global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ import 'focus-visible';
 import React from 'react';
 import MenuLayout from './src/components/layouts/menu';
 import { MenuProvider } from './src/context/MenuContext';
+import './src/global.module.css';
 
 export const wrapRootElement = ({ element }) => (
   <MenuProvider>{element}</MenuProvider>
