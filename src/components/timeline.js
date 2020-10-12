f38d6445d22edd16bd27b2fbf6e3e862e8ac8792
Joni Rämö <joni.ramo.91@gmail.com>
2020-10-12 17:08:14 +0300

    Add key for timeline items

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -8,7 +8,7 @@ export default props => {
       <CenterTitle>{props.title}</CenterTitle>
       {props.timelineItems.map(item => {
         return (
-          <div className={styles.timelineItem}>
+          <div key={item.title} className={styles.timelineItem}>
             <div className={styles.timelineTitle}>{item.title}</div>
             <div className={styles.timelinePoint} />
             <div className={styles.timelineLine} />
