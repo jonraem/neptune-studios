9d07525becaa7cf73f74cf3bb8eec6e5ac0144d4
Joni Rämö <joni.ramo.91@gmail.com>
2020-11-12 15:48:17 +0200

    Updates

---
+++ component---src-pages-404-js-35bab6aa969f8a0d7bfd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,t,e){"use strict";e.r(t);var o=e("q1tI"),u=e.n(o);t.default=function(){return u.a.createElement("div",null,"404 Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-35bab6aa969f8a0d7bfd.js.map
