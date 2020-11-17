c823e570a602bb47bb894379a3aaee869d270ade
Joni Rämö <joni.ramo.91@gmail.com>
2020-11-17 16:10:26 +0200

    Use active style helper for header links navigation

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,24 +4,17 @@ import { ReactComponent as Hamburger } from '../../assets/svg/hamburger.svg';
 import { ReactComponent as NeptuneStudios } from '../../assets/svg/neptune-studios--white.svg';
 import MenuContext from '../../context/MenuContext';
 import useMedia from '../../hooks/useMedia';
+import {
+  getActiveStyleForNavigation,
+} from '../../utils/helpers';
 import buttonStyles from '../Buttons/buttons.module.css';
 import styles from './header.module.css';
 
 export default props => {
   const { setIsOpen } = useContext(MenuContext);
   const isMobile = useMedia('(max-width: 480px)');
-
-  const getActiveBorder = pathName => {
-    if (pathName === '/') {
-      if (props.currentPath === pathName) {
-        return { textDecoration: 'line-through' };
-      } else return {};
-    } else {
-      if (props.currentPath.includes(pathName)) {
-        return { textDecoration: 'line-through' };
-      } else return {};
-    }
-  };
+  const getActiveStyleForNavigationWithCurrentPath = pathName =>
+    getActiveStyleForNavigation(pathName, props.currentPath);
 
   return (
     <div className={styles.header}>
@@ -35,27 +28,31 @@ export default props => {
         </button>
       ) : (
         <div className={styles.links}>
-          <Link to="/" className={styles.link} style={getActiveBorder('/')}>
+          <Link
+            to="/"
+            className={styles.link}
+            style={getActiveStyleForNavigationWithCurrentPath('/')}
+          >
             Main
           </Link>
-          <Link
-            to="/work"
-            className={styles.link}
-            style={getActiveBorder('/work')}
-          >
-            Work
-          </Link>
+            <Link
+              to="/work"
+              className={styles.link}
+              style={getActiveStyleForNavigationWithCurrentPath('/work')}
+            >
+              Work
+            </Link>
           <Link
             to="/about"
             className={styles.link}
-            style={getActiveBorder('/about')}
+            style={getActiveStyleForNavigationWithCurrentPath('/about')}
           >
             About
           </Link>
           <Link
             to="/contact"
             className={styles.link}
-            style={getActiveBorder('/contact')}
+            style={getActiveStyleForNavigationWithCurrentPath('/contact')}
           >
             Contact
           </Link>
