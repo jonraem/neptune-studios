2d3329e3c24905fdb5a53a40bda6c88c9b65a184
Joni Rämö <joni.ramo.91@gmail.com>
2021-02-08 17:49:39 +0200

    Add mobile styles to timeline

--- src/components/Timeline/timeline.js
+++ src/components/Timeline/timeline.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import useMedia from '../../hooks/useMedia';
 import CenterTitle from '../CenterTitle/centerTitle';
 import FullWidthGrey from '../FullWidth/grey';
 import styles from './timeline.module.css';
 
 const Timeline = props => {
+  const isMobile = useMedia('(max-width: 480px)');
   return (
     <div className={styles.timeline}>
       <FullWidthGrey />
       <CenterTitle>{props.title}</CenterTitle>
       {props.timelineItems.map(item => {
-        return (
-          <div key={item.title} className={styles.item}>
-            <div className={styles.title}>{item.title}</div>
-            <div className={styles.point} />
-            <div className={styles.line} />
-            <div className={styles.description}>{item.description}</div>
-          </div>
-        );
+        if (isMobile) {
+          return (
+            <div key={item.title} className={styles.item}>
+              <div className={styles.point} />
+              <div className={styles.line} />
+              <div className={styles.wrapper}>
+                <div className={styles.title}>{item.title}</div>
+                <div className={styles.description}>{item.description}</div>
+              </div>
+            </div>
+          );
+        } else
+          return (
+            <div key={item.title} className={styles.item}>
+              <div className={styles.title}>{item.title}</div>
+              <div className={styles.point} />
+              <div className={styles.line} />
+              <div className={styles.description}>{item.description}</div>
+            </div>
+          );
       })}
     </div>
   );
