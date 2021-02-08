7c786574bcb8e3ee2c5b5c351a004c84e1df219f
Joni Rämö <joni.ramo.91@gmail.com>
2021-02-08 20:58:37 +0200

    Add gatsby-ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,13 @@
+import 'focus-visible';
+import React from 'react';
+import MenuLayout from './src/components/layouts/menu';
+import { MenuProvider } from './src/context/MenuContext';
+import './src/global.module.css';
+
+export const wrapRootElement = ({ element }) => (
+  <MenuProvider>{element}</MenuProvider>
+);
+
+export const wrapPageElement = ({ element, props }) => (
+  <MenuLayout currentPath={props.location.pathname}>{element}</MenuLayout>
+);
