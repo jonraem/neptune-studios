9cd99bd5fa9bac69ef83072988e721bd77546f46
Joni Rämö <joni.ramo.91@gmail.com>
2021-04-25 20:32:38 +0300

    Add initial 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { ReactComponent as NotFoundTitle } from '../assets/svg/404.svg';
+import pagesStyles from './404.module.css';
 
-const NotFound = () => <div>404 Not Found</div>;
+const NotFound = () => (
+  <div className={pagesStyles.page}>
+    <NotFoundTitle />
+    <p className={pagesStyles.description}>
+      Diving bell has reached the unexplored trenches!
+      <br />
+      <Link to="/">Return to the surface?</Link>
+    </p>
+  </div>
+);
 
 export default NotFound;
