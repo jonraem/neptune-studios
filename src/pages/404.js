115e0fde7867a10aa415b62149f257ec5bcd52c3
Joni Rämö <joni.ramo.91@gmail.com>
2021-05-15 20:51:26 +0300

    Add angler fish to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { ReactComponent as NotFoundTitle } from '../assets/svg/404.svg';
+import { ReactComponent as NotFoundTitle } from '../assets/svg/404-title.svg';
+import { ReactComponent as NotFoundIllustration } from '../assets/svg/404-illustration.svg';
 import pagesStyles from './404.module.css';
 
 const NotFound = () => (
   <div className={pagesStyles.page}>
-    <NotFoundTitle />
+    <NotFoundTitle className={pagesStyles.title} />
     <p className={pagesStyles.description}>
       Diving bell has reached the unexplored trenches!
       <br />
       <Link to="/">Return to the surface?</Link>
     </p>
+    <NotFoundIllustration className={pagesStyles.illustration} />
   </div>
 );
 
